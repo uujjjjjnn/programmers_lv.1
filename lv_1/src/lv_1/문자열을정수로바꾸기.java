c76562e81ac669b7e9f5a69a6e4c9c71a9e540da
uujjjjjnn <uujjjjjnn@gmail.com>
2023-11-07 23:14:39 +0900

    1107 commit

---
+++ lv_1/src/lv_1/문자열을정수로바꾸기.java
@@ -0,0 +1,15 @@
+package lv_1;
+
+public class 문자열을정수로바꾸기 {
+	
+	class Solution {
+	    public int solution(String s) {
+	        int answer = 0;
+	        
+	        answer = Integer.parseInt(s);         
+	        
+	        return answer;
+	    }
+	}
+
+}
