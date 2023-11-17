b700a100885ce9b5fb8d9cb05b7c3c4331d20fb0
uujjjjjnn <uujjjjjnn@gmail.com>
2023-11-17 18:05:14 +0900

    1117 commit

--- lv_1/src/lv_1/자연수뒤집어배열로만들기.java
+++ lv_1/src/lv_1/자연수뒤집어배열로만들기.java
@@ -8,10 +8,11 @@ public class 자연수뒤집어배열로만들기 {
 	        int[] answer = new int[a.length()];
 	        
 	        int cnt = 0;
-	        while(n!=0) {
-	            answer[cnt] = (int)n%10;
+	        while(n>0) {
+	            answer[cnt] = (int)(n%10);
 	            n /= 10;
 	            cnt++;
+	            System.out.print(n);
 	        }
 	        
 	        return answer;
